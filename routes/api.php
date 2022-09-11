36f7cede34029c636e8e3f2b5a0d3259c7568a9d
Alex <xaam1@ya.ru>
2022-09-11 17:40:33 +0300

    sdsd

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/', function () {
-
-
-
-});
+Route::post('/', [\App\Http\Controllers\WalletController::class, 'index']);
