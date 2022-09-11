fe6d0d447cc655b75f20549f4b30227ce12c0296
Alex <xaam1@ya.ru>
2022-09-11 14:22:11 +0300

    aa

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,75 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-  $response = Http::post('http://dappsgate.com:88/api/v2/CreateAccount', [
-      'Name' => 'PrivTest02',
-      'PrivKey' => '7E7C353B062FBCABACE786277A33557AFC8DB53CE088094B44013DC49CFE15BD',
-      'Currency' => '0',
-      'Confirm' => '1',
-  ]);
-
-  $accountFields['result']      = $response->json('result');
-  $accountFields['text']        = $response->json('text');
-  $accountFields['TxID']        = $response->json('TxID');
-  $accountFields['BlockNum']    = $response->json('BlockNum');
-  $accountFields['TrNum']       = $response->json('TrNum');
-
-
     return view('welcome');
+});
+Route::post('/', function () {
+    echo 'qqqq';
+    $bot_token = '5717737611:AAHWBoCma3HawHq1xkxtRvniwPBUxMNAbK0'; // токен вашего бота
+    $data = file_get_contents('php://input'); // весь ввод перенаправляем в $data
+    $data = json_decode($data, true);
+    file_put_contents(storage_path().'/message.txt', '$data');
+
+
+
+
+
+// Основной код: получаем сообщение, что юзер отправил боту и
+// заполняем переменные для дальнейшего использования
+    if (!empty($data['message']['text'])) {
+        $chat_id = $data['message']['from']['id'];
+        $user_name = $data['message']['from']['username'];
+        $first_name = $data['message']['from']['first_name'];
+        $last_name = $data['message']['from']['last_name'];
+        $text = trim($data['message']['text']);
+        $text_array = explode(" ", $text);
+
+        if ($text == '/help') {
+            $text_return = "Привет, $first_name $last_name, вот команды, что я понимаю:
+/help - список команд
+/about - о нас
+";
+            message_to_telegram($bot_token, $chat_id, $text_return);
+        }
+        elseif ($text == '/about') {
+            $text_return = "verysimple_bot:
+Я пример самого простого бота для телеграм, написанного на простом PHP.
+Мой код можно скачивать, дополнять, исправлять. Код доступен в этой статье:
+https://www.novelsite.ru/kak-sozdat-prostogo-bota-dlya-telegram-na-php.html
+";
+            message_to_telegram($bot_token, $chat_id, $text_return);
+        }
+
+    }
+
+
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+// функция отправки сообщени в от бота в диалог с юзером
+function message_to_telegram($bot_token, $chat_id, $text, $reply_markup = '')
+{
+    $ch = curl_init();
+    $ch_post = [
+        CURLOPT_URL => 'https://api.telegram.org/bot' . $bot_token . '/sendMessage',
+        CURLOPT_POST => TRUE,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_TIMEOUT => 10,
+        CURLOPT_POSTFIELDS => [
+            'chat_id' => $chat_id,
+            'parse_mode' => 'HTML',
+            'text' => $text,
+            'reply_markup' => $reply_markup,
+        ]
+    ];
+
+    curl_setopt_array($ch, $ch_post);
+    curl_exec($ch);
+}
+
+
 
 require __DIR__.'/auth.php';
